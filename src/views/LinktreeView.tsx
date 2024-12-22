810cfe4b7982893c55f5ce4ff72c6f78f4436a34
Chris666 <g.a_cristo42@hotmail.com>
2024-12-21 22:39:22 -0600

    Agregar enlaces

--- src/views/LinktreeView.tsx
+++ src/views/LinktreeView.tsx
@@ -1,6 +1,89 @@
+import { useEffect, useState } from "react"
+import DevTreeInput from "../components/DevTreeInput";
+import { social } from "../data/social";
+import { isValidUrl } from "../utils";
+import { toast } from "sonner";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateProfile } from "../api/DevTreeAPI";
+import { SocialNetwork, User } from "../types";
 
 export default function LinktreeView() {
+
+  const [devTreeLinks, setDevTreeLinks] = useState(social);
+
+  const queryClient = useQueryClient();
+  const user : User = queryClient.getQueryData(['user'])!;
+  const {mutate} = useMutation({
+    mutationFn: updateProfile,
+    onError: (error) => {
+      toast.error(error.message);
+    },
+    onSuccess: () => {
+      toast.success('Actualizado correctamente');
+    }
+  })
+
+  useEffect(() => {
+    const updateData = devTreeLinks.map( item => {
+      const userLinks = JSON.parse(user.links).find( (link: SocialNetwork) => link.name === item.name);
+      if(userLinks){
+        return {...item, url: userLinks.url, enabled: userLinks.enabled}
+      }
+      return item;
+    })
+    setDevTreeLinks(updateData);
+  }, []);
+
+  const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const updatedLinks = devTreeLinks.map( link => link.name === e.target.name ? { ...link, url: e.target.value } : link );
+    setDevTreeLinks(updatedLinks);
+
+    queryClient.setQueryData(['user'], (prevData: User) => {
+      return {
+        ...prevData,
+        links: JSON.stringify(updatedLinks)
+      }
+    })
+  }
+
+  const handleEnableLink = (socialNetwork: string) => {
+    const updatedLinks = devTreeLinks.map( link => {
+      if (link.name === socialNetwork) {
+        if(isValidUrl(link.url)){
+          return { ...link, enabled: !link.enabled }
+        } else {
+          toast.error('Invalid URL');
+        }
+      }
+      return link;
+    });
+    setDevTreeLinks(updatedLinks);
+
+    queryClient.setQueryData(['user'], (prevData: User) => {
+      return {
+        ...prevData,
+        links: JSON.stringify(updatedLinks)
+      }
+    })
+
+  }
+
   return (
-    <div>LinktreeView</div>
+    <>
+      <div className="space-y-5">
+          {devTreeLinks.map( item => (
+              <DevTreeInput 
+                key={item.name}
+                item={item}
+                handleUrlChange={handleUrlChange}
+                handleEnableLink={handleEnableLink}
+              />
+          ))}
+          <button
+            className="bg-cyan-400 p-2 text-lg w-full uppercase text-slate-600 rounded-lg font-bold"
+            onClick={() => mutate(queryClient.getQueryData(['user'])!)}
+          >Guardar Cambios</button>
+      </div>
+    </>
   )
 }
--- src/views/ProfileView.tsx
+++ src/views/ProfileView.tsx
@@ -24,7 +24,7 @@ export default function ProfileView() {
             toast.error(error.message);
         },
         onSuccess: (data) => {
-            toast.success(data)
+            toast.success(data);
             queryClient.invalidateQueries({queryKey: ['user']})
         }
     });
@@ -52,60 +52,63 @@ export default function ProfileView() {
         }
 
     const handleUserProfileForm = (formData: ProfileForm) => {
-        updateProfileMutation.mutate(formData);
+        const user : User = queryClient.getQueryData(['user'])!;
+        user.description = formData.description;
+        user.handle = formData.handle;
+        updateProfileMutation.mutate(user);
     }
 
-  return (
-      <form 
-          className="bg-white p-10 rounded-lg space-y-5"
-          onSubmit={handleSubmit(handleUserProfileForm)}
-      >
-          <legend className="text-2xl text-slate-800 text-center">Editar Información</legend>
-          <div className="grid grid-cols-1 gap-2">
-              <label
-                  htmlFor="handle"
-              >Handle:</label>
-              <input
-                  type="text"
-                  className="border-none bg-slate-100 rounded-lg p-2"
-                  placeholder="handle o Nombre de Usuario"
-                  {...register('handle', {
-                    required: "El nombre de usuario es obligatorio"
-                  })}
-              />
-              {errors.handle && <ErrorMessage>{errors.handle.message}</ErrorMessage>}
-          </div>
+    return (
+        <form 
+            className="bg-white p-10 rounded-lg space-y-5"
+            onSubmit={handleSubmit(handleUserProfileForm)}
+        >
+            <legend className="text-2xl text-slate-800 text-center">Editar Información</legend>
+            <div className="grid grid-cols-1 gap-2">
+                <label
+                    htmlFor="handle"
+                >Handle:</label>
+                <input
+                    type="text"
+                    className="border-none bg-slate-100 rounded-lg p-2"
+                    placeholder="handle o Nombre de Usuario"
+                    {...register('handle', {
+                        required: "El nombre de usuario es obligatorio"
+                    })}
+                />
+                {errors.handle && <ErrorMessage>{errors.handle.message}</ErrorMessage>}
+            </div>
 
-          <div className="grid grid-cols-1 gap-2">
-              <label
-                  htmlFor="description"
-              >Descripción:</label>
-              <textarea
-                  className="border-none bg-slate-100 rounded-lg p-2"
-                  placeholder="Tu Descripción"
-                  {...register('description')}
-              />
-          </div>
+            <div className="grid grid-cols-1 gap-2">
+                <label
+                    htmlFor="description"
+                >Descripción:</label>
+                <textarea
+                    className="border-none bg-slate-100 rounded-lg p-2"
+                    placeholder="Tu Descripción"
+                    {...register('description')}
+                />
+            </div>
 
-          <div className="grid grid-cols-1 gap-2">
-              <label
-                  htmlFor="handle"
-              >Imagen:</label>
-              <input
-                  id="image"
-                  type="file"
-                  name="handle"
-                  className="border-none bg-slate-100 rounded-lg p-2"
-                  accept="image/*"
-                  onChange={ handleChange }
-              />
-          </div>
+            <div className="grid grid-cols-1 gap-2">
+                <label
+                    htmlFor="handle"
+                >Imagen:</label>
+                <input
+                    id="image"
+                    type="file"
+                    name="handle"
+                    className="border-none bg-slate-100 rounded-lg p-2"
+                    accept="image/*"
+                    onChange={ handleChange }
+                />
+            </div>
 
-          <input
-              type="submit"
-              className="bg-cyan-400 p-2 text-lg w-full uppercase text-slate-600 rounded-lg font-bold cursor-pointer"
-              value='Guardar Cambios'
-          />
-      </form>
-  )
+            <input
+                type="submit"
+                className="bg-cyan-400 p-2 text-lg w-full uppercase text-slate-600 rounded-lg font-bold cursor-pointer"
+                value='Guardar Cambios'
+            />
+        </form>
+    )
 }
